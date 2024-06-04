90624f11d7101deb16c3bc7e5c639afbc9f1c238
Daniel <massao.tsuzuki@gmail.com>
2024-06-04 00:56:47 -0300

    Inicio

---
+++ src/test/java/danieltsuzuk/com/github/amedigital/AmeDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package danieltsuzuk.com.github.amedigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmeDigitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
